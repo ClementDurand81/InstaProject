b183045457dba6a232656b611f18d36ee535b212
ClementDurand81 <clement.durand.cci@gmail.com>
2023-11-07 11:21:31 +0100

    5

--- Components/PostItem.js
+++ Components/PostItem.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
 
-const PostItem =  props  => {
+const PostItem = props => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{props.post.title}</Text>
       <Text style={styles.user}>Posté par {props.post.user}</Text>
       <Image source={{ uri: props.post.photoLink }} style={styles.image} />
-      <Text style={styles.description}>{props.post.description}</Text>
+      {showDescription ? (
+        <View>
+          <Text style={styles.description}>{props.post.description}</Text>
+          <Button
+            title="Masquer la description"
+            onPress={toggleDescription}
+          />
+        </View>
+      ) : (
+        <Button
+          title="Afficher la description"
+          onPress={toggleDescription}
+        />
+      )}
     </View>
   );
 };
--- Components/Posts.js
+++ Components/Posts.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { Text, View, FlatList } from 'react-native';
-import posts from '../Helpers/Data.js';
+import React, {useState, useEffect} from 'react';
+import {View, FlatList} from 'react-native';
 import PostItem from './PostItem';
+import { getPosts } from './../API/postAPI.js'
 
 const Posts = () => {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    getPosts().then(res => {
+      setPosts(res.data)
+  })
+  }, [])
   return (
     <View>
       <FlatList
         data={posts}
         renderItem={({ item }) => <PostItem post={item}></PostItem>}/>
     </View>
-  );
-};
+  )
+}
 
 export default Posts;
