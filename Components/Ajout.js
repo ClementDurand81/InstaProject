dbde60e9855285604229ccde87a92a97de184152
ClementDurand81 <clement.durand.cci@gmail.com>
2023-11-28 11:55:49 +0100

    6

--- Components/Ajout.js
+++ Components/Ajout.js
@@ -1,48 +1,73 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function AddPostScreen({ navigation }) {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const titleRef = useRef('');
+  const descriptionRef = useRef('');
+  const [image, setImage] = useState(null);
 
-  const takePhoto = () => {
+  const handleAddPost = () => {
+    console.log('Titre :', titleRef.current);
+    console.log('Description :', descriptionRef.current);
+    console.log('Image :', image);
+  }
 
+  const pickImageFromGallery = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
   };
 
-  const pickUpPhotoFromPhone = () => {
+  const takePhoto = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
+    if (!result.cancelled) {
+      setImage(result.uri);
     }
-
-  const photoOption = { maxHeight: 1000,maxWidth: 1000,
-    mediaType: 'photo'};
-    
-  const handleAddPost = () => {
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Ajout Publication</Text>
-      <Text style={styles.label}>Titre</Text>
-      <TextInput
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-        style={styles.input}
-      />
-      <Text style={styles.label}>Description de la publication :</Text>
-      <TextInput
-        value={description}
-        onChangeText={(text) => setDescription(text)}
-        multiline
-        style={styles.input}
-      />
+      <Text style={styles.title}>Ajout de Publication</Text>
+      <View style={styles.formContainer}>
+        <Text style={styles.label}>Titre</Text>
+        <TextInput
+          onChangeText={(text) => {
+            titleRef.current = text;
+          }}
+          style={styles.input}
+        />
+        <Text style={styles.label}>Description de la publication :</Text>
+        <TextInput
+          onChangeText={(text) => {
+            descriptionRef.current = text;
+          }}
+          multiline
+          style={[styles.input, styles.multilineInput]}
+        />
+        {image && <Image source={{ uri: image }} style={styles.imagePreview} />}
+      </View>
       <TouchableOpacity onPress={handleAddPost} style={styles.addButton}>
         <Text style={styles.addButtonText}>Ajouter</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.galleryButton}>
-        <Text style={styles.buttonText}>Galerie</Text>
+      <TouchableOpacity onPress={pickImageFromGallery} style={styles.actionButton}>
+        <Text style={styles.buttonText}>Choisir depuis la Galerie</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.photoButton}>
-        <Text style={styles.buttonText}>Prendre une photo</Text>
+      <TouchableOpacity onPress={takePhoto} style={styles.actionButton}>
+        <Text style={styles.buttonText}>Prendre une Photo</Text>
       </TouchableOpacity>
     </View>
   );
@@ -53,24 +78,44 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F8F8F8',
     padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
-    fontSize: 24,
-    fontWeight: '',
+    fontSize: 28,
+    fontWeight: 'bold',
     color: 'black',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  formContainer: {
+    width: '100%',
     marginBottom: 20,
   },
   label: {
-    fontSize: 16,
+    fontSize: 18,
     color: 'gray',
-    marginBottom: 10,
+    marginBottom: 5,
   },
   input: {
     backgroundColor: 'white',
-    borderBottomWidth: 1,
+    borderWidth: 1,
     borderColor: 'gray',
+    borderRadius: 5,
     padding: 10,
-    marginBottom: 20,
+    marginBottom: 15,
+    fontSize: 16,
+  },
+  multilineInput: {
+    height: 100,
+    textAlignVertical: 'top',
+  },
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginBottom: 15,
+    resizeMode: 'cover',
+    borderRadius: 5,
   },
   addButton: {
     backgroundColor: '#03CEA4',
@@ -81,23 +126,20 @@ const styles = StyleSheet.create({
   },
   addButtonText: {
     color: 'white',
-    fontSize: 16,
+    fontSize: 18,
+    fontWeight: 'bold',
   },
-  galleryButton: {
+  actionButton: {
     backgroundColor: '#03CEA4',
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
+    width: '80%',
     marginBottom: 10,
-  },
-  photoButton: {
-    backgroundColor: '#03CEA4',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
   },
   buttonText: {
     color: 'white',
     fontSize: 16,
+    fontWeight: 'bold',
   },
 });
