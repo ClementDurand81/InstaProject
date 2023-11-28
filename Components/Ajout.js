f60656681ffa9597f85df72e3f8ac0abe296523e
ClementDurand81 <clement.durand.cci@gmail.com>
2023-11-28 09:12:22 +0100

    Encore

--- Components/Ajout.js
+++ Components/Ajout.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { launchCamera } from 'react-native-image-picker';
 
 export default function AddPostScreen({ navigation }) {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
+  const takePhoto = () => {
+    launchCamera(photoOption)
+    .then(currentPhoto => {
+      setPhoto(currentPhoto.assets[0])
+    })
+  };
+
+  const photoOption = { maxHeight: 1000,maxWidth: 1000,
+    mediaType: 'photo'};
+    
   const handleAddPost = () => {
   };
 
